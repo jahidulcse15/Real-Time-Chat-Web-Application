6303fbd9d201fdb93547f61068223406e6bf38a7
Md.Jahidul Islam <jahidulcse15@gmail.com>
2025-08-09 14:21:44 +0600

    Add files via upload

---
+++ ConnectedUsers.cs
@@ -0,0 +1,7 @@
+﻿namespace Real_Time_Chat_Web_Application
+{
+    public class ConnectedUsers
+    {
+        public static List<string> myConnectedUsers = new List<string>();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+using Real_Time_Chat_Web_Application.Hubs;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSignalR();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.MapHub<ConnectedHub>("/ConnectedHub");
+app.Run();
